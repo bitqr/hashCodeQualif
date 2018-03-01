89f3aa3f2ac8c50193862de542a0b54e69cdc5db
Ahmed Med <ahmed.mohamed@decisionbrain.com>
2018-03-01 19:39:54 +0100

    Input file parser

--- src/main/java/hashcode/InputStructure.java
+++ src/main/java/hashcode/InputStructure.java
@@ -4,77 +4,81 @@ package hashcode;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //To be changed according to the input.in structure description
 public class InputStructure {
 	
-	public class Entity1 {
-		public String field1;
-		public int field2;
+	
+	
+	public Map<String,Integer> inputDefinitions = new HashMap<String, Integer>();
+	public List<RideInput> rides = new ArrayList<RideInput>();
+	
+	public class RideInput {
+		public Position startPosition;
+		public Position finishPosition;
+		public int earliestStart;
+		public int latestFinish;
+		
 		
 		@Override
 		public String toString() {
-			return "Entity1: {field1: "+field1+", field2: "+field2+"}";
+			return "RideInput:\nstartPosition:"+startPosition.toString()+", finishPosition:"+finishPosition.toString()+",\nearliestStart: "+earliestStart+",\nlatestFinish:"+latestFinish;
 		}
+		
 	}
 	
-	public class Entity2 {
-		public double field1;
+	public class Position {
+		public int row;
+		public int column;
 		
 		@Override
 		public String toString() {
-			return "Entity2: {field1: "+field1+"}";
+			return "["+row+","+column+"]";
 		}
 	}
 	
-	public List<Entity1> entities1;
-	public List<Entity2> entities2;
 	
-	public InputStructure() throws IOException {
-		FileHandler.parseInput("src/resources/input.in");
+	
+	public InputStructure(String fileName) throws IOException {
+		FileHandler.parseInput("src/resources/"+fileName);
 		int lineOffset = 0;
 		int lineIndex = 0;
-		
-		//SECTION 1: number of different entities
-		int numberOfEntities1 = FileHandler.getIntAt(0,0);
-		int numberOfEntities2 = FileHandler.getIntAt(0,1);
-		entities1 = new ArrayList<Entity1>(numberOfEntities1);
-		entities2 = new ArrayList<Entity2>(numberOfEntities2);
-		
-		//SECTION 2: values for first type of entities
-		lineOffset = lineIndex + 1;
-		for(lineIndex = 0; lineIndex < numberOfEntities1; lineIndex ++) {
-			Entity1 entity1 = new Entity1();
-			entity1.field1 = FileHandler.getStringAt(lineOffset + lineIndex, 0);
-			entity1.field2 = FileHandler.getIntAt(lineOffset + lineIndex, 1);
-			entities1.add(entity1);
-		}
-		
-		//SECTION 3: values for second type of entitites
-		lineOffset = lineOffset + lineIndex;
-		for(lineIndex = 0; lineIndex < numberOfEntities2; lineIndex ++) {
-			Entity2 entity2 = new Entity2();
-			entity2.field1 = FileHandler.getDoubleAt(lineOffset + lineIndex, 0);
-			entities2.add(entity2);
+
+		inputDefinitions.put("rows", FileHandler.getIntAt(0,0));
+		inputDefinitions.put("columns", FileHandler.getIntAt(0,1));
+		inputDefinitions.put("vehicles", FileHandler.getIntAt(0,2));
+		inputDefinitions.put("rides", FileHandler.getIntAt(0,3));
+		inputDefinitions.put("bonus", FileHandler.getIntAt(0,4));
+		inputDefinitions.put("steps", FileHandler.getIntAt(0,5));
+		lineOffset = 1;
+		for (lineIndex=1; lineIndex<inputDefinitions.get("rides")+lineOffset; lineIndex++) {
+			RideInput rideInput = new RideInput();
+			rideInput.startPosition = new Position();
+			rideInput.startPosition.row = FileHandler.getIntAt(lineIndex,0);
+			rideInput.startPosition.column = FileHandler.getIntAt(lineIndex,1);
+			rideInput.finishPosition = new Position();
+			rideInput.finishPosition.row = FileHandler.getIntAt(lineIndex,2);
+			rideInput.finishPosition.column= FileHandler.getIntAt(lineIndex,3);
+			rideInput.earliestStart = FileHandler.getIntAt(lineIndex,4);
+			rideInput.latestFinish = FileHandler.getIntAt(lineIndex,5);
+			rides.add(rideInput);
 		}
 				
 	}
 	
 	@Override
 	public String toString() {
-		StringBuilder result = new StringBuilder("InputStructure:");
-		result.append("\nentities1:[");
-		for(Entity1 e1: entities1) {
-			result.append(e1.toString()+" ");
+		StringBuilder sb = new StringBuilder("InputFile Structure:\n");
+		for(String key:inputDefinitions.keySet()) {
+			sb.append("\n"+key+":"+inputDefinitions.get(key));
 		}
-		result.append("\n]\nentities2:[");
-		for(Entity2 e2: entities2) {
-			result.append(e2.toString()+" ");
+		for(RideInput rideInput : rides) {
+			sb.append("\n"+rideInput.toString());
 		}
-		result.append("\n]");
-		return result.toString();
-		
+		return sb.toString();
 	}
 	
 
