bd4bc005ff014cc039c36cb0f0f9557b11f2294b
patriceo <oms.patrice@gmail.com>
2018-03-01 19:16:00 +0000

    Update Solution.java

--- src/main/java/hashcode/Solution.java
+++ src/main/java/hashcode/Solution.java
@@ -1,9 +1,6 @@
 package hashcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Abdel on 01/03/2018.
