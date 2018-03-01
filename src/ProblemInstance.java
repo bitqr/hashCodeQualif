c88ae6d54cfe24ec0fce14dc319a4febccfcce4f
Abdel <abdel.bitar@decisionbrain.com>
2018-03-01 12:46:47 +0100

    Initial commit - feel free to add IO methods and scala code

---
+++ src/ProblemInstance.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Abdel on 01/03/2018.
+ */
+public class ProblemInstance {
+
+    public ProblemInstance(String fileName){
+
+    }
+
+    public Solution solve(){
+        Solution solution = new Solution();
+
+        return solution;
+    }
+
+
+}
---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Abdel on 01/03/2018.
+ */
+public class Solution {
+
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        return str.toString();
+    }
+
+    public void write(){
+
+    }
+
+    public void readInitialSolution(String fileName){
+
+    }
+}
